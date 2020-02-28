bf88994166ef9c19ba9e63e9ebd4e546eaeb4732
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-02-27 23:54:13 -0600

    Added Large Mushrooms to Swamps

--- src/main/java/com/farcr/swampexpansion/core/registries/SwampExFeatures.java
+++ src/main/java/com/farcr/swampexpansion/core/registries/SwampExFeatures.java
@@ -4,11 +4,19 @@ import com.farcr.swampexpansion.common.world.gen.feature.CattailsFeature;
 import com.farcr.swampexpansion.common.world.gen.feature.HangingWillowLeavesFeature;
 import com.farcr.swampexpansion.common.world.gen.feature.WillowTreeFeature;
 
+import net.minecraft.world.biome.Biome;
+import net.minecraft.world.biome.DefaultBiomeFeatures;
+import net.minecraft.world.gen.GenerationStage;
 import net.minecraft.world.gen.feature.Feature;
+import net.minecraft.world.gen.feature.IFeatureConfig;
+import net.minecraft.world.gen.feature.MultipleRandomFeatureConfig;
 import net.minecraft.world.gen.feature.NoFeatureConfig;
+import net.minecraft.world.gen.placement.AtSurfaceWithExtraConfig;
+import net.minecraft.world.gen.placement.Placement;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.ForgeRegistries;
 
 @Mod.EventBusSubscriber(modid = "swampexpansion", bus = Mod.EventBusSubscriber.Bus.MOD)
 @SuppressWarnings("rawtypes")
@@ -24,9 +32,21 @@ public class SwampExFeatures {
                 CATTAILS, HANGING_WILLOW_LEAVES, WILLOW_TREE
         );
     }
+    
+    public static void addMiscSwampFeatures() {
+        ForgeRegistries.BIOMES.getValues().forEach(SwampExFeatures::generate);
+    }
+
+	public static void generate(Biome biome) {
+        if (biome.getCategory() == Biome.Category.SWAMP) {
+            biome.addFeature(GenerationStage.Decoration.VEGETAL_DECORATION, Biome.createDecoratedFeature(Feature.RANDOM_SELECTOR, new MultipleRandomFeatureConfig(new Feature[]{Feature.FANCY_TREE}, new IFeatureConfig[] {IFeatureConfig.NO_FEATURE_CONFIG}, new float[] {0.5F}, Feature.NORMAL_TREE, IFeatureConfig.NO_FEATURE_CONFIG), Placement.COUNT_EXTRA_HEIGHTMAP, new AtSurfaceWithExtraConfig(0, 0.5F, 1)));
+            DefaultBiomeFeatures.addHugeMushrooms(biome);
+        }
+    }
 
     public static void generateFeatures() {
         CattailsFeature.addFeature();
         HangingWillowLeavesFeature.addFeature();
+        SwampExFeatures.addMiscSwampFeatures();
     }
 }
