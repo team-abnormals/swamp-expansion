b1679172e18be926a8d258f66b25723847628c84
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-03-28 11:17:59 -0500

    Registered Kelp Rolls & Rice Cakes

--- src/main/java/com/farcr/swampexpansion/core/registries/SwampExFoods.java
+++ src/main/java/com/farcr/swampexpansion/core/registries/SwampExFoods.java
@@ -3,6 +3,17 @@ package com.farcr.swampexpansion.core.registries;
 import net.minecraft.item.Food;
 
 public class SwampExFoods {
-	public static Food BITTER_BERRIES = new Food.Builder().hunger(2).saturation(0.0F).build();
-	public static Food BITTER_BERRY_JUICE = new Food.Builder().hunger(2).saturation(1.5F).build();
+	public static Food RICE_BALL 			= new Food.Builder().hunger(2).saturation(0.0F).build();
+
+	public static Food COD_KELP_ROLL 		= new Food.Builder().hunger(2).saturation(0.0F).build();
+	public static Food CLOWNFISH_KELP_ROLL 	= new Food.Builder().hunger(2).saturation(0.0F).build();
+	public static Food CRAB_KELP_ROLL 		= new Food.Builder().hunger(2).saturation(0.0F).build();
+	public static Food PIKE_KELP_ROLL 		= new Food.Builder().hunger(2).saturation(0.0F).build();
+	public static Food CAVEFISH_KELP_ROLL 	= new Food.Builder().hunger(2).saturation(0.0F).build();
+
+	public static Food SALMON_RICE_CAKE 	= new Food.Builder().hunger(2).saturation(0.0F).build();
+	public static Food PUFFERFISH_RICE_CAKE = new Food.Builder().hunger(2).saturation(0.0F).build();
+	public static Food LIONFISH_RICE_CAKE 	= new Food.Builder().hunger(2).saturation(0.0F).build();
+
+	public static Food SQUID_INK_RISOTTO 	= new Food.Builder().hunger(2).saturation(0.0F).build();
 }
--- src/main/java/com/farcr/swampexpansion/core/registries/SwampExItems.java
+++ src/main/java/com/farcr/swampexpansion/core/registries/SwampExItems.java
@@ -10,28 +10,37 @@ import net.minecraft.item.BucketItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Items;
+import net.minecraft.item.SoupItem;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
+@SuppressWarnings("deprecation")
 @Mod.EventBusSubscriber(modid = "swampexpansion", bus = Mod.EventBusSubscriber.Bus.MOD)
 public class SwampExItems {
 	public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, "swampexpansion");
 	
 	public static RegistryObject<Item> MUD_BALL = RegistryUtils.createItem("mud_ball", () -> new MudBallItem(new Item.Properties().group(ItemGroup.MATERIALS)));
 	public static RegistryObject<Item> MUD_BRICK = RegistryUtils.createItem("mud_brick", () -> new Item(new Item.Properties().group(ItemGroup.MATERIALS)));
-	//public static RegistryObject<Item> WILLOW_SIGN = RegistryUtils.createItem("willow_sign", () -> new SignItem(new Item.Properties().group(ItemGroup.DECORATIONS), SwampExBlocks.WILLOW_SIGN, SwampExBlocks.WILLOW_SIGN_WALL));
-	@SuppressWarnings("deprecation")
+//	public static RegistryObject<Item> WILLOW_SIGN = RegistryUtils.createItem("willow_sign", () -> new SignItem(new Item.Properties().group(ItemGroup.DECORATIONS), SwampExBlocks.WILLOW_SIGN, SwampExBlocks.WILLOW_SIGN_WALL));
 	public static RegistryObject<Item> MUD_BUCKET = RegistryUtils.createItem("mud_bucket", () -> new BucketItem(SwampExFluids.MUD, new Item.Properties().containerItem(Items.BUCKET).maxStackSize(1)));
 	public static RegistryObject<Item> WILLOW_BOAT = RegistryUtils.createItem("willow_boat", () -> new WillowBoatItem(WillowBoatEntity.Type.WILLOW, new Item.Properties().group(ItemGroup.TRANSPORTATION).maxStackSize(1)));
 	
 	public static RegistryObject<Item> CATTAIL_SEEDS = RegistryUtils.createItem("cattail_seeds", () -> new BlockNamedItem(SwampExBlocks.CATTAIL_SPROUTS.get(), new Item.Properties().group(ItemGroup.MATERIALS)));
+	public static RegistryObject<Item> RICE = RegistryUtils.createItem("rice", () -> new Item(new Item.Properties().group(ItemGroup.MISC)));
+	public static RegistryObject<Item> RICE_BALL = RegistryUtils.createItem("rice_ball", () -> new Item(new Item.Properties().food(SwampExFoods.RICE_BALL).group(ItemGroup.FOOD)));
 	
-	//public static RegistryObject<Item> BITTER_BERRIES = RegistryUtils.createItem("bitter_berries", () -> new BlockNamedItem(SwampExBlocks.BITTER_BERRY_BUSH.get(), new Item.Properties().food(SwampExFoods.BITTER_BERRIES)));
-	//public static RegistryObject<Item> BITTER_BERRY_JUICE = RegistryUtils.createItem("bitter_berry_juice", () -> new BitterBerryJuiceItem(new Item.Properties().food(SwampExFoods.BITTER_BERRY_JUICE)));
-	//public static RegistryObject<Item> BITTER_BERRY_PIPS = RegistryUtils.createItem("bitter_berry_pips", () -> new BlockNamedItem(SwampExBlocks.BITTER_BERRY_BUSH_PIPS.get(), new Item.Properties()));
-			
-	/*"berry_good", () -> new BlockNamedItem(null, new Item.Properties().food(SwampExFoods.BITTER_BERRIES).group(ItemGroup.FOOD)), 
-	 *"berry_good", () -> new BlockNamedItem(SwampExBlocks.BITTER_BERRY_BUSH_PIPS.get(), new Item.Properties()), */
+	public static RegistryObject<Item> COD_KELP_ROLL = RegistryUtils.createItem("cod_kelp_roll", () -> new Item(new Item.Properties().food(SwampExFoods.COD_KELP_ROLL).group(ItemGroup.FOOD)));
+	public static RegistryObject<Item> CLOWNFISH_KELP_ROLL = RegistryUtils.createItem("clownfish_kelp_roll", () -> new Item(new Item.Properties().food(SwampExFoods.CRAB_KELP_ROLL).group(ItemGroup.FOOD)));
+	public static RegistryObject<Item> CRAB_KELP_ROLL = RegistryUtils.createItem("crab_kelp_roll", () -> new Item(new Item.Properties().food(SwampExFoods.CRAB_KELP_ROLL).group(ItemGroup.FOOD)));
+	public static RegistryObject<Item> PIKE_KELP_ROLL = RegistryUtils.createItem("pike_kelp_roll", () -> new Item(new Item.Properties().food(SwampExFoods.PIKE_KELP_ROLL).group(ItemGroup.FOOD)));
+	public static RegistryObject<Item> CAVEFISH_KELP_ROLL = RegistryUtils.createItem("cavefish_kelp_roll", () -> new Item(new Item.Properties().food(SwampExFoods.RICE_BALL).group(ItemGroup.FOOD)));
+
+	public static RegistryObject<Item> SALMON_RICE_CAKE = RegistryUtils.createItem("salmon_rice_cake", () -> new Item(new Item.Properties().food(SwampExFoods.SALMON_RICE_CAKE).group(ItemGroup.FOOD)));
+	public static RegistryObject<Item> PUFFERFISH_RICE_CAKE = RegistryUtils.createItem("pufferfish_rice_cake", () -> new Item(new Item.Properties().food(SwampExFoods.PUFFERFISH_RICE_CAKE).group(ItemGroup.FOOD)));
+	public static RegistryObject<Item> LIONFISH_RICE_CAKE = RegistryUtils.createItem("lionfish_rice_cake", () -> new Item(new Item.Properties().food(SwampExFoods.LIONFISH_RICE_CAKE).group(ItemGroup.FOOD)));
+	
+	public static RegistryObject<Item> SQUID_INK_RISOTTO = RegistryUtils.createItem("squid_ink_risotto", () -> new SoupItem(new Item.Properties().food(SwampExFoods.SQUID_INK_RISOTTO).group(ItemGroup.FOOD)));
+
 }
