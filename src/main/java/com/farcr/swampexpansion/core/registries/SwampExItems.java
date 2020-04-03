c523b116194c4f1ecc7c7b21bef1424f3466a826
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-04-03 14:07:28 -0500

    fixed some stuff

--- src/main/java/com/farcr/swampexpansion/core/registries/SwampExItems.java
+++ src/main/java/com/farcr/swampexpansion/core/registries/SwampExItems.java
@@ -11,6 +11,7 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Items;
 import net.minecraft.item.SoupItem;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
@@ -32,14 +33,14 @@ public class SwampExItems {
 	public static RegistryObject<Item> RICE_BALL = RegistryUtils.createItem("rice_ball", () -> new Item(new Item.Properties().food(SwampExFoods.RICE_BALL).group(ItemGroup.FOOD)));
 	
 	public static RegistryObject<Item> COD_KELP_ROLL = RegistryUtils.createItem("cod_kelp_roll", () -> new Item(new Item.Properties().food(SwampExFoods.COD_KELP_ROLL).group(ItemGroup.FOOD)));
-	public static RegistryObject<Item> CLOWNFISH_KELP_ROLL = RegistryUtils.createItem("clownfish_kelp_roll", () -> new Item(new Item.Properties().food(SwampExFoods.CRAB_KELP_ROLL).group(ItemGroup.FOOD)));
-	public static RegistryObject<Item> CRAB_KELP_ROLL = RegistryUtils.createItem("crab_kelp_roll", () -> new Item(new Item.Properties().food(SwampExFoods.CRAB_KELP_ROLL).group(ItemGroup.FOOD)));
-	public static RegistryObject<Item> PIKE_KELP_ROLL = RegistryUtils.createItem("pike_kelp_roll", () -> new Item(new Item.Properties().food(SwampExFoods.PIKE_KELP_ROLL).group(ItemGroup.FOOD)));
-	public static RegistryObject<Item> CAVEFISH_KELP_ROLL = RegistryUtils.createItem("cavefish_kelp_roll", () -> new Item(new Item.Properties().food(SwampExFoods.RICE_BALL).group(ItemGroup.FOOD)));
+	public static RegistryObject<Item> CLOWNFISH_KELP_ROLL = RegistryUtils.createItem("clownfish_kelp_roll", () -> new Item(new Item.Properties().food(SwampExFoods.CLOWNFISH_KELP_ROLL).group(ItemGroup.FOOD)));
+	public static RegistryObject<Item> CRAB_KELP_ROLL = RegistryUtils.createItem("crab_kelp_roll", () -> new Item(new Item.Properties().food(SwampExFoods.CRAB_KELP_ROLL).group(ModList.get().isLoaded("quark") ? ItemGroup.FOOD : null)));
+	public static RegistryObject<Item> PIKE_KELP_ROLL = RegistryUtils.createItem("pike_kelp_roll", () -> new Item(new Item.Properties().food(SwampExFoods.PIKE_KELP_ROLL).group(ModList.get().isLoaded("upgrade_aquatic") ? ItemGroup.FOOD : null)));
+	public static RegistryObject<Item> CAVEFISH_KELP_ROLL = RegistryUtils.createItem("cavefish_kelp_roll", () -> new Item(new Item.Properties().food(SwampExFoods.CAVEFISH_KELP_ROLL).group(ModList.get().isLoaded("maby") ? ItemGroup.FOOD : null)));
 
 	public static RegistryObject<Item> SALMON_RICE_CAKE = RegistryUtils.createItem("salmon_rice_cake", () -> new Item(new Item.Properties().food(SwampExFoods.SALMON_RICE_CAKE).group(ItemGroup.FOOD)));
 	public static RegistryObject<Item> PUFFERFISH_RICE_CAKE = RegistryUtils.createItem("pufferfish_rice_cake", () -> new Item(new Item.Properties().food(SwampExFoods.PUFFERFISH_RICE_CAKE).group(ItemGroup.FOOD)));
-	public static RegistryObject<Item> LIONFISH_RICE_CAKE = RegistryUtils.createItem("lionfish_rice_cake", () -> new Item(new Item.Properties().food(SwampExFoods.LIONFISH_RICE_CAKE).group(ItemGroup.FOOD)));
+	public static RegistryObject<Item> LIONFISH_RICE_CAKE = RegistryUtils.createItem("lionfish_rice_cake", () -> new Item(new Item.Properties().food(SwampExFoods.LIONFISH_RICE_CAKE).group(ModList.get().isLoaded("upgrade_aquatic") ? ItemGroup.FOOD : null)));
 	
 	public static RegistryObject<Item> SQUID_INK_RISOTTO = RegistryUtils.createItem("squid_ink_risotto", () -> new SoupItem(new Item.Properties().food(SwampExFoods.SQUID_INK_RISOTTO).group(ItemGroup.FOOD)));
 
