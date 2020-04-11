ea8f57e009698cfaa098b3efad05253424fbf8b0
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-04-11 10:32:59 -0500

    More food values

--- src/main/java/com/farcr/swampexpansion/core/registries/SwampExFoods.java
+++ src/main/java/com/farcr/swampexpansion/core/registries/SwampExFoods.java
@@ -9,7 +9,7 @@ public class SwampExFoods {
 	
 	// saturation value = (hunger * saturation * 2)
 	
-	public static Food RICE_BALL 			= new Food.Builder().hunger(2).saturation(0.2F).build(); //temporary
+	public static Food RICE_BALL 			= new Food.Builder().hunger(4).saturation(0.5F).build(); //0.5
 
 	public static Food COD_KELP_ROLL 		= new Food.Builder().hunger(5).saturation(0.7F).fastToEat().build(); //0.70
 	public static Food CLOWNFISH_KELP_ROLL 	= new Food.Builder().hunger(6).saturation(1.3F).fastToEat().build(); //1.25
@@ -21,5 +21,5 @@ public class SwampExFoods {
 	public static Food PUFFERFISH_RICE_CAKE = new Food.Builder().hunger(6).saturation(0.3F).fastToEat().effect(new EffectInstance(Effects.WEAKNESS, 600), 1.0F).build(); //0.25
 	public static Food LIONFISH_RICE_CAKE 	= new Food.Builder().hunger(5).saturation(0.9F).fastToEat().effect(new EffectInstance(Effects.SLOWNESS, 600), 1.0F).build(); //0.90
 
-	public static Food SQUID_INK_RISOTTO 	= new Food.Builder().hunger(2).saturation(0.2F).build(); //temporary
+	public static Food SQUID_INK_RISOTTO 	= new Food.Builder().hunger(8).saturation(0.9F).build(); //0.94
 }
