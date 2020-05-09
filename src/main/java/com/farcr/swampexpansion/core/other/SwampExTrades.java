c9591f083070869c1bc44d9a4b283295d43d5b1b
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-05-08 21:41:42 -0500

    Added some more Villager Trades

--- src/main/java/com/farcr/swampexpansion/core/other/SwampExTrades.java
+++ src/main/java/com/farcr/swampexpansion/core/other/SwampExTrades.java
@@ -4,6 +4,8 @@ import com.farcr.swampexpansion.core.registry.SwampExBlocks;
 import com.farcr.swampexpansion.core.registry.SwampExItems;
 import com.farcr.swampexpansion.core.util.TradeUtils;
 
+import net.minecraft.entity.merchant.villager.VillagerProfession;
+import net.minecraftforge.event.village.VillagerTradesEvent;
 import net.minecraftforge.event.village.WandererTradesEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -15,5 +17,13 @@ public class SwampExTrades {
 	public static void onWandererTradesEvent(WandererTradesEvent event) {
 		event.getGenericTrades().add(new TradeUtils.ItemsForEmeraldsTrade(SwampExItems.CATTAIL_SEEDS.get(), 1, 1, 6, 1));
 		event.getGenericTrades().add(new TradeUtils.ItemsForEmeraldsTrade(SwampExBlocks.WILLOW_SAPLING.get(), 5, 1, 8, 1));
+		event.getGenericTrades().add(new TradeUtils.ItemsForEmeraldsTrade(SwampExItems.RICE.get(), 1, 1, 12, 1));
 	}
+	
+	@SubscribeEvent
+	public static void onVillagerTradesEvent(VillagerTradesEvent event) {
+		if(event.getType() == VillagerProfession.FARMER) {
+			event.getTrades().get(1).add(new TradeUtils.EmeraldsForItemsTrade(SwampExItems.RICE.get(), 18, 1, 6, 1));
+		}
+    }
 }
