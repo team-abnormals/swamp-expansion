1eac9189cd65fd3ca1c1632215181d526095f9c4
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2020-05-15 01:22:40 -0500

    Fixed some crashes & naming scheme

--- src/main/java/com/farcr/swampexpansion/core/registry/SwampExTileEntities.java
+++ src/main/java/com/farcr/swampexpansion/core/registry/SwampExTileEntities.java
@@ -1,6 +1,5 @@
 package com.farcr.swampexpansion.core.registry;
 
-import com.farcr.swampexpansion.common.block.MudVaseBlock;
 import com.farcr.swampexpansion.common.tile.MudVaseTileEntity;
 import com.farcr.swampexpansion.core.SwampExpansion;
 import com.teamabnormals.abnormals_core.core.utils.RegistryHelper;
@@ -8,14 +7,11 @@ import com.teamabnormals.abnormals_core.core.utils.RegistryHelper;
 import net.minecraft.block.Block;
 import net.minecraft.tileentity.TileEntityType;
 import net.minecraftforge.fml.RegistryObject;
-import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.fml.common.Mod;
 
+@Mod.EventBusSubscriber(modid = SwampExpansion.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class SwampExTileEntities {
 	public static final RegistryHelper HELPER = SwampExpansion.REGISTRY_HELPER;
 
-	public static final RegistryObject<TileEntityType<MudVaseTileEntity>> MUD_VASE = HELPER.createTileEntity("mud_vase", MudVaseTileEntity::new, () -> collectMudVase());
-	
-	private static Block[] collectMudVase() {
-		return ForgeRegistries.BLOCKS.getValues().stream().filter(block -> block instanceof MudVaseBlock).toArray(Block[]::new);
-	}
+	public static final RegistryObject<TileEntityType<MudVaseTileEntity>> MUD_VASE = HELPER.createTileEntity("mud_vase", MudVaseTileEntity::new, () -> new Block[] {SwampExBlocks.MUD_VASE.get()});
 }
